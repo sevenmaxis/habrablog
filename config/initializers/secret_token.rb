b7f8fabc1c49a09116227e56a0d63b05db8ed871
GitLabHQ Admin User <admin@local.host>
2012-07-18 21:31:24 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habrablog::Application.config.secret_token = '5a0d15d6cbd4b2d57055046070a4d355a3e13f6a97a2411f3755cbaa77a3df2e50686c688b8a4262d4c393152bb52bb8194e362a7603fc8fd5072cdbfda51fba'
